4d7970c762c500f766187fda9f520fb1f7a3ef75
david <david>
2020-02-12 12:16:11 +0100

    first commit

---
+++ src/main/java/com/meep/vehicles/VehicleResourcesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.meep.vehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleResourcesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleResourcesServiceApplication.class, args);
+	}
+
+}
