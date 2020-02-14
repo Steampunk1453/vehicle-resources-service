f5487118e5b4035a6616422f4bf267e07ec3f092
david <david>
2020-02-14 23:59:28 +0100

    scheduler method with delay fixed to get vehicle resource list

---
+++ src/main/java/com/meep/vehicles/service/VehicleResourcesService.java
@@ -0,0 +1,9 @@
+package com.meep.vehicles.service;
+
+import com.meep.vehicles.service.dto.Vehicle;
+
+import java.util.List;
+
+public interface VehicleResourcesService {
+    List<Vehicle> getVehicles();
+}
